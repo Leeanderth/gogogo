a6e00cb7f65db0a4ebc2e3170f3b36850df098c5
liteng <l.like.lxr@gmail.com>
2026-07-13 15:56:47 +0800

    initial commit

---
+++ gowiki/wiki.go
@@ -0,0 +1,95 @@
+// go:build ignore
+
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, err
+}
+
+// func main() {
+// 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+// 	p1.save()
+// 	p2, _ := loadPage("TestPage")
+// 	fmt.Println(string(p2.Body))
+// }
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Here we will extract the page title from the Request,
+		// and call the provided handler 'fn'
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		log.Println("----", m)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view.html", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit.html", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
--- hello/hello.go
+++ hello/hello.go
@@ -1,31 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"example.com/greetings"
-)
-
-// import "rsc.io/quote"
+import "fmt"
 
 func main() {
-	// 设置预定义Logger的属性，包括
-	// 日志条目前缀和禁用打印的标志
-	//  时间、源文件和行号.
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
-
-	names := []string{"golang", "", "java"}
-
-	messages, err := greetings.Hellos(names)
-	// log.Println(messages)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	// for _, message := range message {
-	// 	fmt.Println(message)
-	// }
-	fmt.Println(messages)
+	fmt.Println("Hello, world.")
 }
